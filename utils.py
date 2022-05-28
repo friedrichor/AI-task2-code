ac48d128484f663aab9d7eaedeee13bdea8f6367
friedrichor <kongfanheng426@163.com>
2022-05-28 13:26:09 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,454 @@
+import os
+import sys
+import json
+import random
+import math
+from tqdm import tqdm
+
+import shutil
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import params
+
+
+# 划分训练集和验证集
+def split_train_val(root: str, val_rate: float = 0.2):
+    random.seed(0)  # 保证随机结果可复现
+    assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
+
+    # 遍历文件夹，一个文件夹对应一个类别
+    classes = [cla for cla in os.listdir(root) if os.path.isdir(os.path.join(root, cla))]
+    # 排序，保证顺序一致
+    classes.sort()
+    # 生成类别名称以及对应的数字索引
+    class_indices = dict((k, v) for v, k in enumerate(classes))
+    json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
+    with open(str(params.ROOT / 'class_indices.json'), 'w') as json_file:
+        json_file.write(json_str)
+
+    train_images_path = []  # 存储训练集的所有图片路径
+    train_images_label = []  # 存储训练集图片对应索引信息
+    val_images_path = []  # 存储验证集的所有图片路径
+    val_images_label = []  # 存储验证集图片对应索引信息
+    every_class_num = []  # 存储每个类别的样本总数
+    # 遍历每个文件夹下的文件
+    for cla in classes:
+        cla_path = os.path.join(root, cla)
+        # 遍历获取supported支持的所有文件路径
+        images = [os.path.join(root, cla, i) for i in os.listdir(cla_path)]
+        # 获取该类别对应的索引
+        image_class = class_indices[cla]
+        # 记录该类别的样本数量
+        every_class_num.append(len(images))
+        # 按比例随机采样验证样本
+        val_path = random.sample(images, k=int(len(images) * val_rate))
+
+        # for mode in ['train', 'val']:
+        #     if os.path.exists(params.ROOT / mode / cla):
+        #         shutil.rmtree(params.ROOT / mode / cla)
+        #     os.makedirs(params.ROOT / mode / cla)
+
+        for img_path in images:
+            if img_path in val_path:  # 如果该路径在采样的验证集样本中则存入验证集
+                val_images_path.append(img_path)
+                val_images_label.append(image_class)
+                # shutil.copy(img_path, params.ROOT / 'train' / cla)
+            else:  # 否则存入训练集
+                train_images_path.append(img_path)
+                train_images_label.append(image_class)
+                # shutil.copy(img_path, params.ROOT / 'val' / cla)
+
+    print("{} images were found in the dataset.".format(sum(every_class_num)))
+    print("{} images for training.".format(len(train_images_path)))
+    print("{} images for validation.".format(len(val_images_path)))
+
+    return train_images_path, train_images_label, val_images_path, val_images_label
+
+
+def train_one_epoch(model, optimizer, data_loader, device, epoch, lr_scheduler):
+    model.train()
+    loss_function = params.loss_function
+    accu_loss = torch.zeros(1).to(device)  # 累计损失
+    accu_num = torch.zeros(1).to(device)  # 累计预测正确的样本数
+    optimizer.zero_grad()
+
+    sample_num = 0
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images, labels = data
+        sample_num += images.shape[0]
+
+        pred = model(images.to(device))
+        pred_classes = torch.max(pred, dim=1)[1]
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+
+        loss = loss_function(pred, labels.to(device))
+        loss.backward()
+        accu_loss += loss.detach()
+
+        data_loader.desc = "[train epoch {}] loss: {:.3f}, acc: {:.3f}, lr: {:.5f}".format(
+            epoch,
+            accu_loss.item() / (step + 1),
+            accu_num.item() / sample_num,
+            optimizer.param_groups[0]["lr"]
+        )
+
+        if not torch.isfinite(loss):
+            print('WARNING: non-finite loss, ending training ', loss)
+            sys.exit(1)
+
+        optimizer.step()
+        optimizer.zero_grad()
+        # update lr
+        lr_scheduler.step()
+
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+
+
+@torch.no_grad()
+def evaluate(model, data_loader, device, epoch):
+    loss_function = params.loss_function
+
+    model.eval()
+
+    accu_num = torch.zeros(1).to(device)  # 累计预测正确的样本数
+    accu_loss = torch.zeros(1).to(device)  # 累计损失
+
+    sample_num = 0
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images, labels = data
+        sample_num += images.shape[0]
+
+        pred = model(images.to(device))
+        pred_classes = torch.max(pred, dim=1)[1]
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+
+        loss = loss_function(pred, labels.to(device))
+        accu_loss += loss
+
+        data_loader.desc = "[valid epoch {}] loss: {:.3f}, acc: {:.3f}".format(
+            epoch,
+            accu_loss.item() / (step + 1),
+            accu_num.item() / sample_num
+        )
+
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+
+
+class FocalLoss(nn.Module):
+    r"""
+        This criterion is a implemenation of Focal Loss, which is proposed in
+        Focal Loss for Dense Object Detection.
+
+            Loss(x, class) = - \alpha (1-softmax(x)[class])^gamma \log(softmax(x)[class])
+
+        The losses are averaged across observations for each minibatch.
+
+        Args:
+            alpha(1D Tensor, Variable) : the scalar factor for this criterion
+            gamma(float, double) : gamma > 0; reduces the relative loss for well-classiﬁed examples (p > .5),
+                                   putting more focus on hard, misclassiﬁed examples
+            size_average(bool): By default, the losses are averaged over observations for each minibatch.
+                                However, if the field size_average is set to False, the losses are
+                                instead summed for each minibatch.
+    """
+
+    def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
+        super(FocalLoss, self).__init__()
+        if alpha is None:
+            self.alpha = Variable(torch.ones(class_num, 1))
+        else:
+            if isinstance(alpha, Variable):
+                self.alpha = alpha
+            else:
+                self.alpha = Variable(alpha)
+        self.gamma = gamma
+        self.class_num = class_num
+        self.size_average = size_average
+
+    def forward(self, inputs, targets):
+        N = inputs.size(0)
+        C = inputs.size(1)
+        P = F.softmax(inputs)
+
+        class_mask = inputs.data.new(N, C).fill_(0)
+        class_mask = Variable(class_mask)
+        ids = targets.view(-1, 1)
+        class_mask.scatter_(1, ids.data, 1.)
+
+        if inputs.is_cuda and not self.alpha.is_cuda:
+            self.alpha = self.alpha.cuda()
+        alpha = self.alpha[ids.data.view(-1)]
+
+        probs = (P * class_mask).sum(1).view(-1, 1)
+
+        log_p = probs.log()
+
+        batch_loss = -alpha * (torch.pow((1 - probs), self.gamma)) * log_p
+
+        if self.size_average:
+            loss = batch_loss.mean()
+        else:
+            loss = batch_loss.sum()
+        return loss
+
+
+def create_lr_scheduler(optimizer,
+                        num_step: int,
+                        epochs: int,
+                        warmup=True,
+                        warmup_epochs=1,
+                        warmup_factor=1e-3,
+                        end_factor=1e-6):
+    assert num_step > 0 and epochs > 0
+    if warmup is False:
+        warmup_epochs = 0
+
+    def f(x):
+        """
+        根据step数返回一个学习率倍率因子，
+        注意在训练开始之前，pytorch会提前调用一次lr_scheduler.step()方法
+        """
+        if warmup is True and x <= (warmup_epochs * num_step):
+            alpha = float(x) / (warmup_epochs * num_step)
+            # warmup过程中lr倍率因子从warmup_factor -> 1
+            return warmup_factor * (1 - alpha) + alpha
+        else:
+            current_step = (x - warmup_epochs * num_step)
+            cosine_steps = (epochs - warmup_epochs) * num_step
+            # warmup后lr倍率因子从1 -> end_factor
+            return ((1 + math.cos(current_step * math.pi / cosine_steps)) / 2) * (1 - end_factor) + end_factor
+
+    return torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=f)
+
+
+def get_params_groups(model: torch.nn.Module, weight_decay: float = 1e-5):
+    # 记录optimize要训练的权重参数
+    parameter_group_vars = {"decay": {"params": [], "weight_decay": weight_decay},
+                            "no_decay": {"params": [], "weight_decay": 0.}}
+
+    # 记录对应的权重名称
+    parameter_group_names = {"decay": {"params": [], "weight_decay": weight_decay},
+                             "no_decay": {"params": [], "weight_decay": 0.}}
+
+    for name, param in model.named_parameters():
+        if not param.requires_grad:
+            continue  # frozen weights
+
+        if len(param.shape) == 1 or name.endswith(".bias"):
+            group_name = "no_decay"
+        else:
+            group_name = "decay"
+
+        parameter_group_vars[group_name]["params"].append(param)
+        parameter_group_names[group_name]["params"].append(name)
+
+    # print("Param groups = %s" % json.dumps(parameter_group_names, indent=2))
+    return list(parameter_group_vars.values())
+
+
+# 获取单个数据集，不划分训练集和验证集
+def read_data(root: str, mode: str):
+    assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
+
+    # 遍历文件夹，一个文件夹对应一个类别
+    classes = [cla for cla in os.listdir(root) if os.path.isdir(os.path.join(root, cla))]
+    # 排序，保证顺序一致
+    classes.sort()
+    # 生成类别名称以及对应的数字索引
+    class_indices = dict((k, v) for v, k in enumerate(classes))
+    json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
+    with open(params.path_json, 'w') as json_file:
+        json_file.write(json_str)
+
+    images_path = []  # 存储训练集的所有图片路径
+    images_label = []  # 存储训练集图片对应索引信息
+    every_class_num = []  # 存储每个类别的样本总数
+    # 遍历每个文件夹下的文件
+    for cla in classes:
+        cla_path = os.path.join(root, cla)
+        # 遍历获取supported支持的所有文件路径
+        images = [os.path.join(root, cla, i) for i in os.listdir(cla_path)]
+        # 获取该类别对应的索引
+        image_class = class_indices[cla]
+        # 记录该类别的样本数量
+        every_class_num.append(len(images))
+
+        for img_path in images:
+            images_path.append(img_path)
+            images_label.append(image_class)
+
+    if mode == 'train':
+        print("{} images for training.".format(sum(every_class_num)))
+    else:  # mode == 'test'
+        print("{} images for validation.".format(sum(every_class_num)))
+
+    return images_path, images_label
+
+
+# 划分训练集和验证集（K折交叉验证）
+def k_fold_read_split_data(root: str, k_i: int, k_fold: int = 3):
+    random.seed(0)  # 保证随机结果可复现
+    assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
+
+    # 遍历文件夹，一个文件夹对应一个类别
+    classes = [cla for cla in os.listdir(root) if os.path.isdir(os.path.join(root, cla))]
+    # 排序，保证顺序一致
+    classes.sort()
+    # 生成类别名称以及对应的数字索引
+    class_indices = dict((k, v) for v, k in enumerate(classes))
+    json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
+    with open(str(params.ROOT / 'class_indices.json'), 'w') as json_file:
+        json_file.write(json_str)
+
+    train_images_path = []  # 存储训练集的所有图片路径
+    train_images_label = []  # 存储训练集图片对应索引信息
+    val_images_path = []  # 存储验证集的所有图片路径
+    val_images_label = []  # 存储验证集图片对应索引信息
+    every_class_num = []  # 存储每个类别的样本总数
+    # 遍历每个文件夹下的文件
+    for cla in classes:
+        cla_path = os.path.join(root, cla)
+        # 遍历获取supported支持的所有文件路径
+        images = [os.path.join(root, cla, i) for i in os.listdir(cla_path)]
+        # 获取该类别对应的索引
+        image_class = class_indices[cla]
+        # 记录该类别的样本数量
+        every_class_num.append(len(images))
+        # 取第k_i份为验证样本
+        every_z_len = len(images) // k_fold
+        val_path = images[every_z_len * k_i: every_z_len * (k_i + 1)]
+
+        for img_path in images:
+            if img_path in val_path:  # 如果该路径在采样的验证集样本中则存入验证集
+                val_images_path.append(img_path)
+                val_images_label.append(image_class)
+            else:  # 否则存入训练集
+                train_images_path.append(img_path)
+                train_images_label.append(image_class)
+
+    print("{} images were found in the dataset.".format(sum(every_class_num)))
+    print("{} images for training.".format(len(train_images_path)))
+    print("{} images for validation.".format(len(val_images_path)))
+    assert len(train_images_path) > 0, "not find data for train."
+    assert len(val_images_path) > 0, "not find data for eval"
+
+    return train_images_path, train_images_label, val_images_path, val_images_label
+
+
+def multi_train_one_epoch(model1, model2, optimizer1, optimizer2, data_loader1, data_loader2, device, epoch,
+                          lr_scheduler1, lr_scheduler2):
+    model1.to(device)
+    model2.to(device)
+    model1.train()
+    model2.train()
+    loss_function = params.loss_function
+    accu_loss = torch.zeros(1).to(device)  # 累计损失
+    accu_num = torch.zeros(1).to(device)  # 累计预测正确的样本数
+    optimizer1.zero_grad()
+    optimizer2.zero_grad()
+
+    sample_num = 0
+    data_loader1 = tqdm(data_loader1, file=sys.stdout)
+    data_loader2 = tqdm(data_loader2, file=sys.stdout)
+    for step, (data1, data2) in enumerate(zip(data_loader1, data_loader2)):
+        images1, labels1 = data1
+        images2, labels2 = data2
+        sample_num += images1.shape[0]
+
+        pred1 = model1(images1.to(device))
+        pred2 = model2(images2.to(device))
+        pred = pred1 * 0.5 + pred2 * 0.5
+        pred_classes = torch.max(pred, dim=1)[1]
+
+        if torch.equal(labels1, labels2):
+            labels = labels1
+        else:
+            continue
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+
+        loss = loss_function(pred, labels.to(device))
+        loss.backward()
+        accu_loss += loss.detach()
+
+        data_loader1.desc = "[train epoch {}] loss: {:.3f}, acc: {:.3f}, lr: {:.5f}".format(
+            epoch,
+            accu_loss.item() / (step + 1),
+            accu_num.item() / sample_num,
+            optimizer1.param_groups[0]["lr"]
+        )
+        data_loader2.desc = "[train epoch {}] loss: {:.3f}, acc: {:.3f}, lr: {:.5f}".format(
+            epoch,
+            accu_loss.item() / (step + 1),
+            accu_num.item() / sample_num,
+            optimizer2.param_groups[0]["lr"]
+        )
+
+        if not torch.isfinite(loss):
+            print('WARNING: non-finite loss, ending training ', loss)
+            sys.exit(1)
+
+        optimizer1.step()
+        optimizer1.zero_grad()
+        # update lr
+        lr_scheduler1.step()
+
+        optimizer2.step()
+        optimizer2.zero_grad()
+        # update lr
+        lr_scheduler2.step()
+
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+
+
+def multi_evaluate(model1, model2, data_loader1, data_loader2, device, epoch):
+    loss_function = params.loss_function
+
+    model1.to(device)
+    model2.to(device)
+    model1.eval()
+    model2.eval()
+
+    accu_num = torch.zeros(1).to(device)  # 累计预测正确的样本数
+    accu_loss = torch.zeros(1).to(device)  # 累计损失
+
+    sample_num = 0
+    data_loader1 = tqdm(data_loader1, file=sys.stdout)
+
+    with torch.no_grad():
+        for step, (data1, data2) in enumerate(zip(data_loader1, data_loader2)):
+            images1, labels1 = data1
+            images2, labels2 = data2
+            sample_num += images1.shape[0]
+
+            pred1 = model1(images1.to(device))
+            pred2 = model2(images2.to(device))
+            pred = pred1 * 0.5 + pred2 * 0.5
+            pred_classes = torch.max(pred, dim=1)[1]
+
+            if torch.equal(labels1, labels2):
+                labels = labels1
+            else:
+                continue
+
+            accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+
+            loss = loss_function(pred, labels.to(device))
+            accu_loss += loss.detach()
+
+            data_loader1.desc = "[valid epoch {}] loss: {:.3f}, acc: {:.3f}".format(
+                epoch,
+                accu_loss.item() / (step + 1),
+                accu_num.item() / sample_num
+            )
+            data_loader2.desc = "[valid epoch {}] loss: {:.3f}, acc: {:.3f}".format(
+                epoch,
+                accu_loss.item() / (step + 1),
+                accu_num.item() / sample_num
+            )
+
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
