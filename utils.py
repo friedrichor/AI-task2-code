f7ac6eb210a20f5f550358fb0ac91070f23362ba
friedrichor <kongfanheng426@163.com>
2022-05-30 09:46:27 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -46,26 +46,65 @@ def split_train_val(root: str, val_rate: float = 0.2):
         # 按比例随机采样验证样本
         val_path = random.sample(images, k=int(len(images) * val_rate))
 
-        # for mode in ['train', 'val']:
-        #     if os.path.exists(params.ROOT / mode / cla):
-        #         shutil.rmtree(params.ROOT / mode / cla)
-        #     os.makedirs(params.ROOT / mode / cla)
+        for mode in ['train', 'val']:
+            if os.path.exists(params.ROOT / mode / cla):
+                shutil.rmtree(params.ROOT / mode / cla)
+            os.makedirs(params.ROOT / mode / cla)
 
         for img_path in images:
             if img_path in val_path:  # 如果该路径在采样的验证集样本中则存入验证集
                 val_images_path.append(img_path)
                 val_images_label.append(image_class)
-                # shutil.copy(img_path, params.ROOT / 'train' / cla)
+                shutil.copy(img_path, params.ROOT / 'val' / cla)
             else:  # 否则存入训练集
                 train_images_path.append(img_path)
                 train_images_label.append(image_class)
-                # shutil.copy(img_path, params.ROOT / 'val' / cla)
+                shutil.copy(img_path, params.ROOT / 'train' / cla)
 
     print("{} images were found in the dataset.".format(sum(every_class_num)))
     print("{} images for training.".format(len(train_images_path)))
     print("{} images for validation.".format(len(val_images_path)))
 
     return train_images_path, train_images_label, val_images_path, val_images_label
+
+
+# 获取单个数据集，不划分训练集和验证集
+def read_data(root: str, mode: str):
+    assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
+
+    # 遍历文件夹，一个文件夹对应一个类别
+    classes = [cla for cla in os.listdir(root) if os.path.isdir(os.path.join(root, cla))]
+    # 排序，保证顺序一致
+    classes.sort()
+    # 生成类别名称以及对应的数字索引
+    class_indices = dict((k, v) for v, k in enumerate(classes))
+    json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
+    with open(params.path_json, 'w') as json_file:
+        json_file.write(json_str)
+
+    images_path = []  # 存储训练集的所有图片路径
+    images_label = []  # 存储训练集图片对应索引信息
+    every_class_num = []  # 存储每个类别的样本总数
+    # 遍历每个文件夹下的文件
+    for cla in classes:
+        cla_path = os.path.join(root, cla)
+        # 遍历获取supported支持的所有文件路径
+        images = [os.path.join(root, cla, i) for i in os.listdir(cla_path)]
+        # 获取该类别对应的索引
+        image_class = class_indices[cla]
+        # 记录该类别的样本数量
+        every_class_num.append(len(images))
+
+        for img_path in images:
+            images_path.append(img_path)
+            images_label.append(image_class)
+
+    if mode == 'train':
+        print("{} images for training.".format(sum(every_class_num)))
+    else:  # mode == 'test'
+        print("{} images for validation.".format(sum(every_class_num)))
+
+    return images_path, images_label
 
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch, lr_scheduler):
@@ -249,45 +288,6 @@ def get_params_groups(model: torch.nn.Module, weight_decay: float = 1e-5):
 
     # print("Param groups = %s" % json.dumps(parameter_group_names, indent=2))
     return list(parameter_group_vars.values())
-
-
-# 获取单个数据集，不划分训练集和验证集
-def read_data(root: str, mode: str):
-    assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
-
-    # 遍历文件夹，一个文件夹对应一个类别
-    classes = [cla for cla in os.listdir(root) if os.path.isdir(os.path.join(root, cla))]
-    # 排序，保证顺序一致
-    classes.sort()
-    # 生成类别名称以及对应的数字索引
-    class_indices = dict((k, v) for v, k in enumerate(classes))
-    json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
-    with open(params.path_json, 'w') as json_file:
-        json_file.write(json_str)
-
-    images_path = []  # 存储训练集的所有图片路径
-    images_label = []  # 存储训练集图片对应索引信息
-    every_class_num = []  # 存储每个类别的样本总数
-    # 遍历每个文件夹下的文件
-    for cla in classes:
-        cla_path = os.path.join(root, cla)
-        # 遍历获取supported支持的所有文件路径
-        images = [os.path.join(root, cla, i) for i in os.listdir(cla_path)]
-        # 获取该类别对应的索引
-        image_class = class_indices[cla]
-        # 记录该类别的样本数量
-        every_class_num.append(len(images))
-
-        for img_path in images:
-            images_path.append(img_path)
-            images_label.append(image_class)
-
-    if mode == 'train':
-        print("{} images for training.".format(sum(every_class_num)))
-    else:  # mode == 'test'
-        print("{} images for validation.".format(sum(every_class_num)))
-
-    return images_path, images_label
 
 
 # 划分训练集和验证集（K折交叉验证）
